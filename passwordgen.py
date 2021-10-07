6f4ffae0ad51ae09404d676485cc44c2f7aa96b4
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-07 11:23:16 -0500

    added comments

--- passwordgen.py
+++ passwordgen.py
@@ -1,13 +1,16 @@
 from random import choice
 
+"""Options for password characters"""
 options = [
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
         'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 1, 2, 3, 4,
         5, 6, 7, 8, 9, '!', '@', '#', '$', '%', '^', '&'
         ]
 
+"""new password"""
 password = []
 
+"""create password with 8 characters"""
 while len(password) < 8:
     result = choice(options)
 
